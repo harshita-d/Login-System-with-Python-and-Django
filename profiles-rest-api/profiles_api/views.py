fa427e0f27e128213dbaa23f5fc580855bd6aaa9
harshita <hrs.dadhich@gmail.com>
2023-08-03 18:21:55 +0530

    added login api

--- profiles-rest-api/profiles_api/views.py
+++ profiles-rest-api/profiles_api/views.py
@@ -5,6 +5,8 @@ from rest_framework import viewsets
 from profiles_api import serializers
 from profiles_api import models
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 from rest_framework import filters
 from profiles_api import permissions
 
@@ -98,3 +100,9 @@ class UserPofileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user Authentication token"""
+    """http://127.0.0.1:8000/api/login/"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
