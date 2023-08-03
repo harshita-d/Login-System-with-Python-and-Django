fb33c0b4820da4a6481fb2a61cd93bfaf677427a
harshita <hrs.dadhich@gmail.com>
2023-08-03 15:45:51 +0530

    search added to user profile

--- profiles-rest-api/profiles_api/views.py
+++ profiles-rest-api/profiles_api/views.py
@@ -5,6 +5,7 @@ from rest_framework import viewsets
 from profiles_api import serializers
 from profiles_api import models
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 from profiles_api import permissions
 
 
@@ -95,3 +96,5 @@ class UserPofileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
